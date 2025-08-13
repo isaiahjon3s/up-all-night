27561e7c01de36bf59aa0e10d6f52f91672fd1a2
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-08-12 22:37:46 -0400

    i need a job this is dumb

---
+++ brushteeth.py
@@ -0,0 +1,16 @@
+import time
+
+def main():
+    print("Welcome to the tooth brushing time counter!, please start brushing your teech")
+    time.sleep(30)
+    print("Switch sides")
+    time.sleep(30)
+    print("Go to the top now")
+    time.sleep(30)
+    print("Switch sides one more time!")
+    time.sleep(30)
+    print("You're done! Good job!")
+
+if __name__ == "__main__":
+    main()
+    
