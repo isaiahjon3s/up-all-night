858f791892d5e8e777c8cbed622d05c92d18e0ab
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-08-28 23:25:01 -0400

    sIx SeVeN

---
+++ find67.py
@@ -0,0 +1,9 @@
+def main():
+    string = input("Enter a string: ")
+    if "67" in string:
+        print("SiX sEvEn")
+    else:
+        print("No SiX sEvEn")
+
+if __name__ == "__main__":
+    main()
