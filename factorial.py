5e7c01c3614da9b6134cf5728ca0f95e3c24bf6c
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-08-29 23:33:51 -0400

    recursive experiment

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def main():
+    n = int(input("Enter a number: "))
+    print(factorial(n))
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+if __name__ == "__main__":
+    main()
