a65933d2b44c301cbd1ce90229c236fb47a9f7f9
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-09-05 23:58:17 -0400

    search.py

---
+++ search.py
@@ -0,0 +1,10 @@
+def main();
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    target = input("Enter a number: ")
+    if target in list:
+        print("Target found")
+    else:
+        print("Target not found")
+
+if __name__ == "__main__":
+    main()
