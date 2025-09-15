ab60e0642583375e3b21ded4e6cad6576c1f54db
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-09-14 23:37:29 -0400

    adds next letter in alphabet to end of string/letter

---
+++ improv.py
@@ -0,0 +1,23 @@
+def main():
+    word = input("Enter a word: ")
+    print(improv(word.strip().replace(" ", "")))
+
+alphabet = list("abcdefghijklmnopqrstuvwxyza")
+
+def improv(word):
+    last_letter = word[-1]
+    next_letter = ""
+    i = 0
+    while i < 28:
+        if alphabet[i] == last_letter:
+            next_letter += alphabet[i+1]
+            break
+        i += 1
+    improvd = f"{word}{next_letter}"
+    return improvd
+
+
+    
+
+if __name__ == "__main__":
+    main()
