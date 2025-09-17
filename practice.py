c4e7a3a2d47b4ce9a702bd81d8c2f4bf847b5b39
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-09-16 23:28:23 -0400

    reminder to go practice tbh

---
+++ practice.py
@@ -0,0 +1,8 @@
+def practice():
+    hours = int(input("How many hours have you practiced today?"))
+    if hours < 24:
+        print("You need to practice more!")
+    else:
+        print("Good job!")
+
+practice()
